e92a64664e3fee99ec5f94d2d1819d6273f58d66
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2013-10-18 17:21:36 -0300

    Add skeleton file skel.c

---
+++ skel.c
@@ -0,0 +1,20 @@
+#include "fractal.h"
+
+/*
+ * Do not compile me, just copy me and fill the functions
+ */
+
+#error
+
+
+int iterative_method (point *p, options *opt) {
+
+}
+
+int close_to_solution (point *p, options *opt) {
+
+}
+
+void method_print (FILE *f, int s, int k, int max, options *opt) {
+
+}
